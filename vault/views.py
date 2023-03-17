bf97f0ae369b2c3c3023a7d819083bbfef5ba1cf
Asher <asher@pop-os.localdomain>
2023-03-17 21:56:58 +0330

    add simple-jwt

--- vault/views.py
+++ vault/views.py
@@ -19,4 +19,13 @@ class RegisterAPI(GenericAPIView):
 
 
 class PasswordAPI(GenericAPIView):
-    pass
+    serializer_class = PasswordSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+
+        return Response({
+            "user": UserSerializer(user, context=self.get_serializer_context()).data,
+        })
